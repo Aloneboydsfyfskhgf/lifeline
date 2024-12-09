a859e98a72d7e304bb9b577047f97c6a785b6bb1
Aloneboydsfyfskhgf <aasifansari18754@gmail.com>
2024-12-09 18:31:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '0') # request channel id ( make
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Ansari:ikra765592@cluster0.op7ki.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
 
